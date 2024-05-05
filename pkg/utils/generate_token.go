8869d6ca92ab1e516e8f0f91b4b633ae12b9041e
awahids <awahid.safhadi@gmail.com>
2024-05-06 00:24:41 +0700

    fix auth middleware

--- pkg/utils/generate_token.go
+++ pkg/utils/generate_token.go
@@ -7,13 +7,15 @@ import (
 	"github.com/dgrijalva/jwt-go"
 )
 
+var jwtSecret = []byte(os.Getenv("JWT_SECRET"))
+
 func GenerateToken(user *models.User) (string, error) {
-	token := jwt.NewWithClaims(jwt.SigningMethodHS512, jwt.MapClaims{
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"user_id": int(user.ID),
 		"email":   user.Email,
 	})
 
-	tokenString, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+	tokenString, err := token.SignedString(jwtSecret)
 	if err != nil {
 		return "", err
 	}
